1824bfff03a5e9a3882b654b1c46f7448f4288f6
Vlad Shakun <cr4vlad@gmail.com>
2020-06-15 18:41:32 +0300

    Asset setup w/ assets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import './style.css';
+import Svg from './hello-world.svg'
+
 const p = document.createElement("p");
 p.innerHTML = 'Hello world!'
+p.classList.add('hello');
 document.body.appendChild(p);
+const svg = new Image();
+svg.src = Svg;
+document.body.appendChild(svg);
--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,21 @@ module.exports = {
     filename: 'script.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
 };
