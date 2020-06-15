5e9a9dcb43030a45cf3a5416d90a4797bceaa2a1
Vlad Shakun <cr4vlad@gmail.com>
2020-06-15 22:43:45 +0300

    Add styled-components & scss modules, changed appearance for testing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
+import styles from './App.module.scss';
 
 function App() {
+  const Hello = styled.p`
+    font-size: 1.5em;
+    color: palevioletred;
+  `;
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.App}>
+      <Hello>Hello, world?..</Hello>
     </div>
   );
 }
